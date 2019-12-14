5ad89169abf6bed29f9c6e51f54788debad85d8f
Albertojo44al <Albertojo44al@unitec.edu>
2019-12-13 20:31:52 -0600

    solo creadores y administradores pueden modificar y eliminar

--- plantilla1.php
+++ plantilla1.php
@@ -8,7 +8,7 @@ $user = $_GET['user'];
 
 $codigoCurso = $_GET['codigo'];
 
-$sql = "SELECT SUBTEMA,INFORMACION, NOMBRE_CURSO, TITULO FROM CURSOS C inner join CONTENIDO CON ON CON.CODIGO_CURSOS=C.CODIGO INNER JOIN SUBTEMAS SUB ON
+$sql = "SELECT USUARIO,INFORMACION, NOMBRE_CURSO, TITULO FROM CURSOS_X_USUARIOS cxc inner join CURSOS C on cxc.CODIGO = c.CODIGO inner join CONTENIDO CON ON CON.CODIGO_CURSOS=C.CODIGO INNER JOIN SUBTEMAS SUB ON
 SUB.CODIGO_CONTENIDO = CON.CODIGO_CONTENIDO WHERE C.CODIGO = $codigoCurso;";
 $query = ibase_query($con,$sql);
 
@@ -17,6 +17,7 @@ while($row = ibase_fetch_object($query)){
     $contador++;
     $nombre = $row->NOMBRE_CURSO;
     $title = $row->TITULO;
+       $creador = $row->USUARIO;
     if($contador ==1){
     $subtema = $row->SUBTEMA;
     $info = $row->INFORMACION;
@@ -40,11 +41,16 @@ echo "
 
 
 
-        <h1><center><b>$title</b></center></h1>
-
-        <div align='right' class='eliminar'>
-        <a href='eliminarCurso.php?codigo=$codigoCurso&&user=$user&&nav=$nav'>Delete Course</a>
+        <h1><center><b>$title</b></center></h1>";
+    if($creador == $user || $nav==1){
+        echo "<div align='right' class='eliminar'>
+        <a href='eliminarCurso.php?codigo=$codigoCurso&&user=$user&&nav=$nav'>Delete Course</a><br><br>
         </div>
+        <div align='right' class='modificar'>
+        <a href='eliminarCurso.php?codigo=$codigoCurso&&user=$user&&nav=$nav'>Modify Course</a>
+        </div>";
+    }
+            echo "
 
          <center>
         <hr width=50%>
@@ -103,6 +109,18 @@ echo "
 }
     .eliminar a:hover{
     background :#C71313;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+    .modificar a{
+    text-decoration: none;
+    color:white;
+    background: #023C4A;
+    padding: 10px 12px;
+    border-radius: 5px;
+    text-align: right;
+}
+    .modificar a:hover{
+    background :#0FB5DD;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
     </style>
--- plantilla3.php
+++ plantilla3.php
@@ -10,7 +10,7 @@ $user = $_GET['user'];
 
 $codigoCurso = $_GET['codigo'];
 
-$sql = "SELECT SUBTEMA,INFORMACION, NOMBRE_CURSO, TITULO FROM CURSOS C inner join CONTENIDO CON ON CON.CODIGO_CURSOS=C.CODIGO INNER JOIN SUBTEMAS SUB ON
+$sql = "SELECT USUARIO,INFORMACION, NOMBRE_CURSO, TITULO FROM CURSOS_X_USUARIOS cxc inner join CURSOS C on cxc.CODIGO = c.CODIGO inner join CONTENIDO CON ON CON.CODIGO_CURSOS=C.CODIGO INNER JOIN SUBTEMAS SUB ON
 SUB.CODIGO_CONTENIDO = CON.CODIGO_CONTENIDO WHERE C.CODIGO = $codigoCurso;";
 $query = ibase_query($con,$sql);
 
@@ -19,6 +19,7 @@ while($row = ibase_fetch_object($query)){
     $contador++;
     $nombre = $row->NOMBRE_CURSO;
     $title = $row->TITULO;
+    $creador = $row->USUARIO;
     if($contador ==1){
     $subtema = $row->SUBTEMA;
     $info = $row->INFORMACION;
@@ -38,14 +39,16 @@ echo "
     </title>
 </head>
     <body style='background-color:#96BDB7'><br><br><br>
-
-
-        <h1><center><b>$title</b></center></h1>
-
-        <div align='right' class='eliminar'>
-        <a href='eliminarCurso.php?codigo=$codigoCurso&&user=$user&&nav=$nav'>Delete Course</a>
+        <h1><center><b>$title</b></center></h1>";
+    if($creador == $user || $nav==1){
+        echo "<div align='right' class='eliminar'>
+        <a href='eliminarCurso.php?codigo=$codigoCurso&&user=$user&&nav=$nav'>Delete Course</a><br><br>
         </div>
-
+        <div align='right' class='modificar'>
+        <a href='eliminarCurso.php?codigo=$codigoCurso&&user=$user&&nav=$nav'>Modify Course</a>
+        </div>";
+    }
+            echo "
          <center>
         <hr width=50%>
         <div class='contenedorp1'>
@@ -63,16 +66,6 @@ echo "
             </div>
         </div>
         </center>
-
-
-        <div class='contBotones'>
-        <div class='eliminar'>
-        <a href='eliminarCurso.php?codigo=$codigoCurso&&nav=$nav'>Delete Course</a>
-        </div>
-        <div class='modificar'>
-
-        </div>
-        </div>
 
     </body>
 </html>";
@@ -110,6 +103,17 @@ echo "
     background :#C71313;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
-
+    .modificar a{
+    text-decoration: none;
+    color:white;
+    background: #023C4A;
+    padding: 10px 12px;
+    border-radius: 5px;
+    text-align: right;
+}
+    .modificar a:hover{
+    background :#0FB5DD;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
 
     </style>
